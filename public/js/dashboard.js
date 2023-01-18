e5dff85d968d8cb0e9c265ddf1dd4630788baa05
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-01-17 22:01:53 -0500

    syncing

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,9 @@
 const games = document.querySelectorAll('.game-link');
 games.forEach(game => {
 
-game.addEventListener('click', function () {
-    const gameId = this.querySelector('p:first-child').innerHTML;
-    console.log(gameId);
-    return document.location.replace('/games/'+ gameId);
-});
+    game.addEventListener('click', function () {
+        const gameId = this.querySelector('p:first-child').innerHTML;
+        console.log(gameId);
+        return document.location.replace('/games/' + gameId);
+    });
 });
